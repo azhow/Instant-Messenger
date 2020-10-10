e0fde8024d7bcfad828fe16cf7e48bd098af8231
adpei <adpei@AUGUSTO-PC>
2020-10-09 21:40:00 -0300

    alterações feita pra arrumar \0 etc

--- Client/main.cpp
+++ Client/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
 {
     //// Write dummy group file
     //{
-    //    std::ofstream wf("/home/lain/Groups/Patata.msg", std::ios::out | std::ios::binary);
+    //    std::ofstream wf("/home/adpeiter/Groups/Patata.msg", std::ios::out | std::ios::binary);
     //    if (!wf) {
     //        std::cout << "Cannot open file!" << std::endl;
     //        return 1;
@@ -55,19 +55,9 @@ int main(int argc, char* argv[])
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
         printf("ERROR opening socket\n");
 
-    struct timeval t;
-    t.tv_sec = 0;
-    t.tv_usec = 0;
-    setsockopt(
-        sockfd,     // Socket descriptor
-        SOL_SOCKET, // To manipulate options at the sockets API level
-        SO_RCVTIMEO,// Specify the receiving or sending timeouts 
-        (const void*)(&t), // option values
-        sizeof(t)
-    );
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(4000);
+    serv_addr.sin_port = htons(PORT);
     serv_addr.sin_addr = *((struct in_addr*)server->h_addr);
     bzero(&(serv_addr.sin_zero), 8);
 
@@ -77,18 +67,19 @@ int main(int argc, char* argv[])
         printf("%d\n", errno);
     }
 
-    int iMode = 0;
-    ioctl(sockfd, FIONBIO, &iMode);
 
     // Login in the server
-    CMessage::sendLoginMessage(sockfd, "Patati", "Patata");
+    CMessage::sendLoginMessage(sockfd, argv[1], argv[2]);
 
     bool isConnectionClosed{ false };
     while (!isConnectionClosed)
     {
         CMessage receivedMessage{ CMessage::readMessageFromSocket(sockfd, isConnectionClosed) };
-        std::cout << receivedMessage.getMessageData() << std::endl;
-        std::cout << "========== END OF MESSAGE ==========" << std::endl;
+        if (!isConnectionClosed)
+        {
+            std::cout << receivedMessage.getMessageData() << std::endl;
+            std::cout << "========== END OF MESSAGE ==========" << std::endl;
+        }
     }
     ///* read from the socket */
     //n = read(sockfd, buffer, 256);
--- Message/header/CMessage.h
+++ Message/header/CMessage.h
@@ -103,12 +103,13 @@ private:
         // Struct constructor
         SMessage(std::time_t timestamp, const std::string& userID,
             const std::string& groupID, const std::string& messageData) :
-            m_header{ SMessageHeader{timestamp, userID, groupID, messageData.size()} }
+            m_header{ SMessageHeader{timestamp, userID, groupID, messageData.size()+1} }
         {
             // Allocate memory for the message data
-            m_data = new char[messageData.size()];
+            m_data = new char[m_header.m_messageSize];
             // Copy message to struct
-            strncpy(m_data, messageData.c_str(), messageData.size());
+            strncpy(m_data, messageData.c_str(), m_header.m_messageSize);
+            m_data[messageData.size()] = '\0';
         }
 
         // Struct constructor
